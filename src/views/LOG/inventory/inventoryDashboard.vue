7f87c3638443685c97b6452d6b5455baea24d84e
Damola Oladeji <darmhoo@hotmail.com>
2020-12-09 15:17:06 +0100

    reusable search done

--- src/views/LOG/inventory/inventoryDashboard.vue
+++ src/views/LOG/inventory/inventoryDashboard.vue
@@ -45,9 +45,18 @@
       <div class="mt-2 mt-lg-3 row attendance-head">
         <div class="col-md-10">
           <resueable-search
-            v-on:childToParent="searchEvent"
-            :stacks="searchColumns"
-          ></resueable-search>
+            @childToParent="prepareList"
+            :url="urlToFetchOrders"
+          >
+           <template #default= "{ searchQuery }">
+                <div class="col-md">
+                    <div>
+                    <label class="form-control-label">Product Name:  </label>
+                    </div>
+                    <input type="text" v-model="searchQuery.productName" class="form-control">
+                </div>
+                </template>
+          </resueable-search>
         </div>
 
         <div class="col-md">
@@ -302,6 +311,7 @@ import Vue2Filters from "vue2-filters";
 import CustomHeader from "../../../components/customHeader";
 import BasePagination from "../../../components/Pagination/BasePagination";
 import InventorySearch from "../../../components/InventorySearch";
+import renewalVue from '../../FSL/renewal/renewal.vue';
 Vue.use(Vue2Filters);
 export default {
   props: {
@@ -432,6 +442,7 @@ export default {
         total,
         prev_page_url,
       } = response.data;
+      console.log('yes',response);
       this.pageParams = Object.assign({}, this.pageParams, {
         current_page,
         first_page_url,
@@ -444,8 +455,8 @@ export default {
         total,
         prev_page_url,
       });
-      this.inventories = response.data.products;
-      this.summary = response.data.summary;
+      this.inventories = response.data.products ? response.data.products : response.data.data.products;
+      this.summary =  response.data.summary ? response.data.summary : response.data.data.summary;
       console.log("opopopp", this.inventories);
       this.OId = from;
       this.$LIPS(false);

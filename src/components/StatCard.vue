b3f21c2d000d7545d2427abd1bcb95c7cdd83ebb
Damola Oladeji <darmhoo@hotmail.com>
2021-08-11 08:51:12 +0100

    done

---
+++ src/components/StatCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card">
+        <div class="card-header">header</div>
+        <div class="card-body">Body</div>        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'StatCard',
+        props:{
+            stat: {
+                type: Object,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Admin/ReportingTool.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div class="row my-3 mx-1">
+            <stat-card :stat="obj" class="col mx-2"></stat-card>
+            <stat-card :stat="obj" class="col mx-2"></stat-card>
+            <stat-card :stat="obj" class="col mx-2"></stat-card>
+        
+        </div>
+
+        <div>
+            <!-- filters -->
+            <div></div>
+            <!-- table -->
+            <div class="m-auto">
+                <table class="table table-responsive w-100">
+                    <thead>
+                        <tr>
+                            <th>Show room</th>
+                            <th>Total Revenue</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Oyo</td>
+                            <td>100</td>
+                        </tr>
+                    </tbody>
+                </table>
+                 
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+import StatCard from '../../components/StatCard.vue'
+
+    export default {
+  components: { StatCard },
+  data () {
+      return {
+          obj: {}
+      }
+  }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

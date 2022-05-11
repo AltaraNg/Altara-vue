9337aae9a1204ab20ebd52a84de11dff041804a4
Adewuyi Taofeeq Olamilekan <36686152+harmlessprince@users.noreply.github.com>
2022-05-11 11:12:01 +0100

    fixed switch css (#332)
    * integrated ban54
    
    * changed selection and added flag
    
    * removed log
    
    * fixed toggle button bug
    
    * fixed conflict
    
    * fixed conflict
    
    * remved math random from keys
    
    * fixed switch css
    
    Co-authored-by: harmlessprince <realolamilekan@gmail>

--- src/components/ToggleButton.vue
+++ src/components/ToggleButton.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="col">
-    <div>
-      <label :for="'toggle_button'+switchName">
-        <input type="checkbox" :id="'toggle_button'+switchName" v-model="checkedValue" class="mr-3" />
-         <span>{{label ? label :'click'}}</span>
-        <span class="toggle__switch"></span>
+    <div class="toggle_container">
+      <label :for="'toggle_button' + switchName" class="toggle__button mr-3">
+        <input
+          type="checkbox"
+          :id="'toggle_button' + switchName"
+          v-model="checkedValue"
+          class="mr-5 toggle_check"
+        />
+        <span class="toggle__switch round"></span>
       </label>
+      <span>{{ label ? label : "click" }}</span>
     </div>
   </div>
 </template>
@@ -21,8 +26,8 @@ export default {
       type: String,
     },
     label: {
-      type: String
-    }
+      type: String,
+    },
   },
 
   data() {
@@ -55,55 +60,62 @@ export default {
 </script>
 
 <style scoped>
+.toggle_container{
+  display: flex;
+  align-items: center;
+}
 .toggle__button {
-   cursor: pointer;
-  text-indent: -9999px;
-  width: 50px;
-  height: 25px;
-  background: grey;
-  display: block;
-  border-radius: 10px;
   position: relative;
-}
-.toggle__button input[type="checkbox"] {
-  opacity: 0;
-  position: absolute;
-  width: 1px;
-  height: 1px;
-}
-.toggle__button .toggle__switch {
   display: inline-block;
-  height: 12px;
-  border-radius: 6px;
-  width: 40px;
-  background: #bfcbd9;
-  box-shadow: inset 0 0 1px #bfcbd9;
-  position: relative;
-  margin-left: 10px;
-  transition: all 0.25s;
+  width: 60px;
+  height: 34px;
 }
-
-.toggle__button .toggle__switch::after,
-.toggle__button .toggle__switch::before {
-  content: "";
-  position: absolute;
-  display: block;
-  height: 18px;
-  width: 18px;
-  border-radius: 50%;
-  left: 0;
-  top: -3px;
-  transform: translateX(0);
-  transition: all 0.25s cubic-bezier(0.5, -0.6, 0.5, 1.6);
-}
-
-.toggle__button .toggle__switch::after {
-  background: #4d4d4d;
-  box-shadow: 0 0 1px #666;
-}
-.toggle__button .toggle__switch::before {
-  background: #4d4d4d;
-  box-shadow: 0 0 0 3px rgba(0, 0, 0, 0.1);
+.toggle__button input {
   opacity: 0;
+  width: 0;
+  height: 0;
+}
+.toggle__switch {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+.toggle__switch:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+/* Rounded sliders */
+.toggle__switch.round {
+  border-radius: 34px;
+}
+
+.toggle__switch.round:before {
+  border-radius: 50%;
+}
+.toggle_check:checked + .toggle__switch {
+  background-color: green;
+}
+
+.toggle_check:focus + .toggle__switch {
+  box-shadow: 0 0 1px green;
+}
+
+.toggle_check:checked + .toggle__switch:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
 }
 </style>

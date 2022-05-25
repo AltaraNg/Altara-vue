cd7a72f1344b525848bc78f6dc8668babc868ee7
Damola Oladeji <darmhoo@hotmail.com>
2022-05-25 09:19:01 +0100

    admin panel

---
+++ src/components/Filters/RequestFilter.vue
@@ -0,0 +1,95 @@
+<template>
+	<div>
+		<div class="d-flex ">
+			<div class="mx-1">
+				<input type="text" placeholder="Phone number" class="mx-input w-100" v-model="phoneNumber" />
+			</div>
+			<div class="mx-1">
+				<input type="text" class="mx-input w-100" placeholder="Customer Name" v-model="customerName" />
+			</div>
+			<div class="w-25 mx-1">
+				<select class="custom-select w-100" v-model="orderType">
+					<option value="" selected>--Order Type--</option>
+					<option value="cash">Cash</option>
+					<option value="product">Product</option>
+				</select>
+			</div>
+			<div class="w-25 mx-1">
+				<select name="status" id="status" class="custom-select w-100" v-model="status">
+					<option value="" selected>--status--</option>
+					<option :value="status.value" v-for="status in statuses">
+						{{ status.value }}
+					</option>
+				</select>
+			</div>
+			<date-picker
+				v-model="fromDate"
+				placeholder="Date From"
+				class="w-25 mx-1"
+				valueType="format"
+			></date-picker>
+			<date-picker
+				v-model="toDate"
+				placeholder="Date To"
+				class="w-25 mx-1"
+				valueType="format"
+			></date-picker>
+		</div>
+        <div class="m-auto text-center my-2">
+		<button class="bg-default p-2 w-25 mx-2 my-2" @click="searchAction">Search</button>
+		<button class="bg-default p-2 w-25 mx-2 my-2" @click="resetAction">Reset</button>
+        </div>
+	</div>
+</template>
+
+<script>
+	import DatePicker from 'vue2-datepicker';
+	import 'vue2-datepicker/index.css';
+
+	export default {
+		data() {
+			return {
+				fromDate: '',
+				toDate: '',
+                status: '',
+                phoneNumber: '',
+                customerName: '',
+                orderType: '',
+                selectedFilters: {},
+				statuses: [
+					{ value: 'pending' },
+					{ value: 'processing' },
+					{ value: 'declined' },
+					{ value: 'accepted' },
+				],
+			};
+		},
+		components: {
+			DatePicker,
+		},
+		methods: {
+			searchAction() {
+                this.fromDate !== '' ? this.selectedFilters.fromDate = this.fromDate : '';
+                this.toDate !== '' ? this.selectedFilters.toDate = this.toDate : '';
+                this.status !== '' ? this.selectedFilters.status = this.status : '';
+                this.orderType !== '' ? this.selectedFilters.orderType = this.orderType : '';
+                this.customerName !== '' ? this.selectedFilters.customerName = this.customerName : '';
+                this.phoneNumber !== '' ? this.selectedFilters.phoneNumber = this.phoneNumber : '';
+				this.$emit('searchParams',this.selectedFilters);
+			},
+            resetAction() {
+                this.fromDate = '';
+				this.toDate = '';
+                this.status = '';
+                this.phoneNumber = '';
+                this.customerName = '';
+                this.orderType = '';
+                this.selectedFilters = {};
+				this.$emit('searchParams',this.selectedFilters);
+                
+            }
+		},
+	};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/FSL/order_request/orderRequest.vue
@@ -0,0 +1,228 @@
+<template>
+	<div>
+		<div class="attendance-body my-3">
+			<h3 class="attendance-title">Order Requests</h3>
+			<div class="card pt-0">
+				<h4 class="card-header">Filter</h4>
+				<div class="card-body">
+					<request-filter @searchParams="search"></request-filter>
+				</div>
+			</div>
+			<div class="">
+				<table class="table table-striped w-100">
+					<thead class="attendance-head bg-default text-white ">
+						<tr class="w-100">
+							<th v-for="header in tableHeaders" class="mx-3  text-center">
+								{{ header }}
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr
+							v-for="(item, index) in orderRequests"
+							data-hoverable="true"
+							class="  text-center "
+						>
+							<td @click="viewRequest(item)" class="pointer">
+								<span
+									class="user text-white"
+									:class="
+										item.status === 'accepted'
+											? 'bg-success'
+											: item.status === 'declined'
+											? 'bg-danger'
+											: 'bg-warning'
+									"
+									>{{ index + OId }}</span
+								>
+							</td>
+							<td>
+								{{
+									(item.customer.first_name + ' ' + item.customer.last_name)
+										| upper
+								}}
+							</td>
+							<td>{{ item.customer.telephone }}</td>
+							<td>{{ formatDate(item.created_at) }}</td>
+							<td>{{ item.order_type }}</td>
+							<td>{{ item.status }}</td>
+							<td>
+								<select
+									v-if="
+										item.status === 'accepted' || item.status === 'declined'
+									"
+									class="custom-select w-50"
+								>
+									<option value="" selected>--select--</option>
+									<option value="">View</option>
+								</select>
+								<select
+									class="custom-select w-50"
+									v-else
+									@change="showModal(item.newStat, item)"
+									v-model="item.newStat"
+								>
+									<option value="" selected>--select--</option>
+									<option value="decline">Decline</option>
+									<option value="accept">Approve</option>
+									
+								</select>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<BasePagination
+				:pageParam="pageMeta"
+				@fetchData="paginationAction"
+				v-if="pageMeta.total > 10"
+			></BasePagination>
+		</div>
+	</div>
+</template>
+
+<script>
+	import moment from 'moment';
+	import { get, patch } from '../../../utilities/api';
+	import BasePagination from '../../../components/Pagination/BasePagination.vue';
+	import RequestFilter from '../../../components/Filters/RequestFilter.vue';
+	import queryParam from '../../../utilities/queryParam';
+
+	const init = () => `/api/order-requests`;
+
+	export default {
+		components: { BasePagination, RequestFilter },
+		data() {
+			return {
+				tableHeaders: [
+					'S/N',
+					'Requested By',
+					'Phone Number',
+					'Date Requested',
+					'Type',
+					'Status',
+					'Action',
+				],
+				requestOrdersApi: '/api/order-requests',
+				orderRequests: [],
+				pageMeta: {},
+				OId: 0,
+			};
+		},
+		mounted() {
+			// this.fetchOrderRequests();
+		},
+		beforeRouteEnter({ query }, from, next) {
+			if (Object.keys(query).length !== 0) {
+				get(init() + queryParam(query))
+					.then((res) => {
+						next((vm) => {
+							vm.orderRequests = res.data.data.order_requests.data;
+							vm.pageMeta = res.data.data.order_requests;
+							vm.OId = vm.pageMeta.from;
+						});
+					})
+					.catch((e) => console.log(e.response.data))
+					.finally(() => next((vm) => {}));
+			} else
+				next((vm) => {
+					vm.fetchOrderRequests();
+				});
+		},
+
+		// beforeRouteUpdate({ query }, from, next) {
+		// 	if (Object.keys(query).length !== 0)
+		// 		get(init() + queryParam(query))
+		// 			.then((res) => {
+		//                 vm.orderRequests = res.data.order_requests.data;
+		// 				vm.pageMeta = res.data.order_requests;
+
+		//             })
+		// 			.catch((e) => console.log(e.response.data))
+		// 			.finally(() => next((vm) => {}));
+		// 	else
+		// 		next((vm) => {
+		// 			vm.fetchOrderRequests();
+		// 		});
+		// },
+
+		methods: {
+			fetchOrderRequests() {
+				this.$LIPS(true);
+				try {
+					get(this.requestOrdersApi).then((response) => {
+						this.orderRequests = response.data.data.order_requests.data;
+						this.pageMeta = response.data.data.order_requests;
+						this.OId = this.pageMeta.from;
+					});
+				} catch (error) {
+				} finally {
+					this.$LIPS(false);
+				}
+			},
+			paginationAction() {
+				this.$LIPS(true);
+
+				let query = this.$route.query;
+				console.log(query);
+				if (Object.keys(query).length !== 0) {
+					get(init() + queryParam(query))
+						.then((res) => {
+							this.orderRequests = res.data.data.order_requests.data;
+							this.pageMeta = res.data.data.order_requests;
+							this.OId = this.pageMeta.from;
+						})
+						.catch((e) => console.log(e.response.data))
+						.finally(() => this.$LIPS(false));
+				}
+			},
+			formatDate(date) {
+				let m = moment(date);
+				return m.fromNow();
+			},
+			viewRequest(item) {
+				console.log(item);
+			},
+			search(payload) {
+				this.$LIPS(true);
+				console.log(payload);
+				this.$router.push('order-request' + queryParam(payload));
+				get(this.requestOrdersApi + queryParam(payload))
+					.then((res) => {
+						this.orderRequests = res.data.data.order_requests.data;
+						this.pageMeta = res.data.data.order_requests;
+						this.OId = this.pageMeta.from;
+					})
+					.catch((e) => console.log(e.response.data))
+					.finally(() => this.$LIPS(false));
+			},
+			showModal(text, req) {
+                console.log(req);
+				this.$swal({
+					title: `${text.toUpperCase()} REQUEST`,
+					text: "Leave a comment",
+					input: 'text',
+					inputValue: '',
+					inputPlaceholder: 'Comments',
+					showCancelButton: true,
+					confirmButtonColor: '#074A74',
+					cancelButtonColor: '#d33',
+					confirmButtonText: `Yes, ${text} it!`,
+					
+				}).then((result) => {
+					console.log(result)
+					let comment = result.value;
+					let url = `api/order-requests/${req.id}/${text}`;
+					patch(url, {reason: comment}).then((res) => {
+						this.$swal('Success', `${text}ed successfully`);
+						this.fetchOrderRequests();
+
+					})
+
+				});
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scoped></style>

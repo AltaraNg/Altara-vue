21e444c367aa8c7ce04bb4779c0c7c22bd76e80b
Damola Oladeji <darmhoo@hotmail.com>
2022-08-02 22:04:40 +0100

    done

--- src/components/NewOrderAmortization.vue
+++ src/components/NewOrderAmortization.vue
@@ -341,6 +341,17 @@
                       v-validate="'required'"
 										/>
 									</div>
+                  <div class="col form-group">
+                    <label for="amount" class="form-control-label"
+                      >Date Paid</label
+                    >
+                    <date-picker
+                      class="w-100"
+                      v-model="lateFeeItem.date_paid"
+                      valueType="format"
+                      placeholder="Date"
+                    ></date-picker>
+                  </div>
 								</div>
 								<div class="row">
 									<div class="col form-group">
@@ -467,7 +478,7 @@ export default {
       }
     },
 	updateLateFee(lateFee, index) {
-				if (this.canEditAmmoPayment && this.standAlone === false) {
+				if (this.standAlone === false) {
 					this.showLateFeeModal = true;
 					this.lateFeeItem = lateFee;
 					this.lateFeeIndex = index;

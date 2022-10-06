7beb779c4b05c94896c336b737878c83933918b5
Damola Oladeji <darmhoo@hotmail.com>
2022-10-06 07:49:41 +0100

    added another color code for late payment

--- src/components/NewOrderAmortization.vue
+++ src/components/NewOrderAmortization.vue
@@ -86,7 +86,10 @@
               <tr class="table-separator status-row">
                 <th>Status</th>
                 <td v-for="armo in amortizationData">
-                  <div v-if="armo.actual_payment_date && armo.actual_amount === armo.expected_amount" class="green">
+                  <div v-if="armo.actual_payment_date && armo.actual_amount === armo.expected_amount && Date.parse(armo.actual_payment_date) <= Date.parse(armo.expected_payment_date)" class="green">
+                    <i class="fa fa-check"></i>
+                  </div>
+                  <div v-else-if="armo.actual_payment_date && armo.actual_amount === armo.expected_amount && Date.parse(armo.actual_payment_date) > Date.parse(armo.expected_payment_date)" class="grey">
                     <i class="fa fa-check"></i>
                   </div>
                   <div
@@ -588,7 +591,14 @@ export default {
   overflow-x: auto;
 }
 .green {
-  color: #00a368;
+  color: #0cd68c;
+  width: 100%;
+  font-size: 1.3em;
+  background: rgba(10, 232, 150, 0.274);
+}
+
+.grey {
+  color: #8fa39b;
   width: 100%;
   font-size: 1.3em;
   background: rgba(0, 163, 104, 0.09);

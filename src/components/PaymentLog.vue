90d95a3f11d9b7ecd590f88b1747a6fafef40cd8
Blessing Onwunali <cheedinmarblessing@gmail.com>
2022-10-19 09:13:48 +0100

    change payload ammortization

--- src/components/PaymentLog.vue
+++ src/components/PaymentLog.vue
@@ -871,7 +871,7 @@ export default {
       this.cardError = false;
       this.salesLogForm.customer_id = this.customerId;
       const data = {
-        ammortization_downpayment:(this.singleRepayment && this.addDownpayment) ? this.singleRepayment : 0,
+        amortization_downpayment:(this.singleRepayment && this.addDownpayment) ? this.singleRepayment : 0,
         customer_id: this.customerId,
         inventory_id: this.selectedProduct.id,
         repayment_duration_id: this.salesLogForm.repayment_duration_id.id,

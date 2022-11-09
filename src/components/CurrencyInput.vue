7ca50d78f87437befd80f88dfbc6727054236ffc
Damola Oladeji <darmhoo@hotmail.com>
2022-11-09 12:13:40 +0100

    fixed bugs

--- src/components/CurrencyInput.vue
+++ src/components/CurrencyInput.vue
@@ -1,23 +1,37 @@
 <template>
-  <input 
-    ref="inputRef" 
-    :value="formattedValue"
-  >
+  <input ref="inputRef" :value="formattedValue" />
 </template>
 
 <script>
+import { watch } from '@vue/composition-api'
 import useCurrencyInput from 'vue-currency-input'
 
 export default {
   name: 'CurrencyInput',
   props: {
     value: Number, // Vue 2: value
-    options: Object
+    options: Object,
   },
-  setup (props) {
-    const { formattedValue, inputRef } = useCurrencyInput(props.options)
+  setup(props) {
+    const { inputRef, formattedValue, setOptions, setValue } = useCurrencyInput(
+      props.options
+    )
+
+    watch(
+      () => props.value, // Vue 2: props.value
+      value => {
+        setValue(value)
+      }
+    )
+
+    watch(
+      () => props.options,
+      options => {
+        setOptions(options)
+      }
+    )
 
     return { inputRef, formattedValue }
-  }
+  },
 }
 </script>

a036f10e6f751a494213ec5c58b63cd0a2c936f0
Omodamola <darmhoo@hotmail.com>
2023-03-03 14:05:56 +0100

    Bnpl-admin (#386)
    * fixed
    
    * worked on the form
    
    * fixed form
    
    * fixed the event save
    
    * Finished vendors
    
    * fixed
    
    * fixed
    
    * done
    
    * done
    
    * fixes
    
    * fixed bugs
    
    * fixed
    
    * done
    
    * fixed
    
    * fixed comments
    
    * removed comment
    
    * fixed
    
    * new fixes
    
    * removed flag
    
    * fixed
    
    * improved modal ui
    
    * fixed the modal

--- src/components/modals/ConfirmModal.vue
+++ src/components/modals/ConfirmModal.vue
@@ -1,50 +1,91 @@
 <template>
-    <div id="changeCustomerManagerModal" class="modal fade w-50 mx-auto">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                
-                <div class="modal-body" id="index" >
-                    <p class="text-center h3">Are you sure?</p>
-                    <div class="row text-center">
-                    <div class="form-group clearfix col">
-                        <button class="btn bg-default" @click="$emit('touched', true)">Yes</button>
-                    </div>
-                    <div class="form-group clearfix col">
-                        <button class="btn bg-info" @click="$emit('touched', false)">No</button>
-                    </div>
-                    </div>
-                </div>
-                
-            </div>
+  <div
+    id="confirmModal"
+    class="modal fade"
+    role="dialog"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content rounded-2 py-0">
+        <div class="py-0">
+          <span
+            type="button"
+            class="close pointer"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true" class="h2 mr-3 text-danger">&times;</span>
+          </span>
         </div>
+
+        <div class="mt-0 row px-4" id="index">
+          <div class="col-2 w-100 text-center my-2 text-right">
+            <!-- Heroicon name: outline/exclamation-triangle -->
+            <svg
+              class="text-danger w-50 text-right"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+              />
+            </svg>
+          </div>
+          <div class="col-10 px-4">
+            <h3 class="my-2 font-weight-bold">Attention!</h3>
+            <p class="text-left text-capitalize h5 font-weight-light ">
+              <slot :customer="{ customer }"></slot>
+            </p>
+            <div class="d-flex justify-content-end mt-4 mx-4">
+              <div class="form-group  mx-2">
+                <button class="btn bg-danger rounded-1 my-0 h2 font-weight-light" data-dismiss="modal">
+                  Cancel
+                </button>
+              </div>
+              <div class="form-group  mx-2">
+                <button class="btn bg-default my-0 h2 font-weight-light" @click="$emit('touched', true)">
+                  Proceed
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            show: {
-                type: Boolean,
-                default: false,
-                required:false
-            }
-        },  
-        
-        methods: {
-            handleModalToggle() {
-                
-                $("#changeCustomerManagerModal").modal("toggle");
-            },
-        },
-        watch: {
-            show() {
-                this.handleModalToggle();
-            
-            }
-        }
-    }
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+    customer: {
+      required: false,
+    },
+  },
+
+  methods: {
+    handleModalToggle() {
+      $('#confirmModal').modal('toggle')
+    },
+  },
+  watch: {
+    show() {
+      this.handleModalToggle()
+    },
+  },
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/views/BNPL/Orders.vue
@@ -0,0 +1,350 @@
+<template>
+  <transition name="fade">
+    <div id="reminder" class="attendance">
+      <custom-header :title="'Orders List'" />
+
+      <div class="mt-2 mt-lg-3 row attendance-head ">
+        <div class="col row" v-if="vendors.length > 0">
+          <resueable-search
+            @childToParent="prepareList"
+            :url="urlToFetchOrders"
+            :showBranch="false"
+            :showDate="false"
+          >
+            <template #default="{ searchQuery }">
+              <div class="col-md">
+                <div>
+                  <label class="form-control-label">Name: </label>
+                </div>
+                <input
+                  type="text"
+                  v-model="searchQuery.name"
+                  class="form-control"
+                />
+              </div>
+            </template>
+          </resueable-search>
+        </div>
+
+        <div class="col float-right mt-5">
+          <button
+            class="btn btn-primary bg-default mt-0 myBtn float-right my-2"
+            @click="createOrder"
+          >
+            <i class="fa fa-plus" aria-hidden="true"></i>
+          </button>
+        </div>
+      </div>
+
+      <div class="mt-5 mb-3 attendance-head" v-if="vendors.length > 0">
+        <div class="w-100 my-5 mx-0 hr"></div>
+        <div class="row px-4 pt-3 pb-4 text-center">
+          <div
+            class="col dark-heading font-weight-bolder"
+            style="max-width: 120px"
+          >
+            S/N
+          </div>
+          <div
+            class="col dark-heading font-weight-bolder"
+            v-for="header in headings"
+          >
+            {{ header }}
+          </div>
+        </div>
+      </div>
+      <div class="tab-content mt-1 attendance-body" v-if="vendors.length > 0">
+        <div
+          class="mb-3 row attendance-item"
+          :key="index"
+          v-for="(vendor, index) in vendors"
+        >
+          <div class="col d-flex align-items-center" style="max-width: 120px">
+            <span class="user mx-auto">{{ index + OId }}</span>
+          </div>
+          <div
+            class="col d-flex align-items-center justify-content-center hover"
+            @click="viewVendor(vendor)"
+          >
+            {{ vendor.full_name }}
+          </div>
+          <div class="col d-flex align-items-center justify-content-center">
+            {{ vendor.staff_id }}
+          </div>
+          <div class="col d-flex align-items-center justify-content-center">
+            {{ vendor.email }}
+          </div>
+          <div class="col d-flex align-items-center justify-content-center">
+            {{ $humanizeDate(vendor.created_at) }}
+          </div>
+        </div>
+      </div>
+      <div v-else>
+        <zero-state
+          :title="'No Orders to view'"
+          :message="'There are currently no Orders'"
+        >
+          <template v-slot:image>
+            <img src="../../assets/thumb-up.png" />
+          </template>
+        </zero-state>
+      </div>
+      <div class="modal fade repayment" id="viewBrand">
+        <div class="modal-dialog " role="document">
+          <div class="modal-content" v-if="showModalContent">
+            <div class="modal-header py-2">
+              <h4>
+                {{ vendorItem.full_name }}
+              </h4>
+              <a aria-label="Close" class="close py-1" data-dismiss="modal">
+                <span aria-hidden="true" class="modal-close text-danger">
+                  <i class="fas fa-times"></i>
+                </span>
+              </a>
+            </div>
+            <div class="modal-body px-5">
+              <div class="table-responsive">
+                <table class="table table-bordered table-striped">
+                  <tbody>
+                    <tr>
+                      <th>Vendor ID</th>
+                      <td>{{ vendorItem.staff_id || 'Not Available' }}</td>
+                    </tr>
+                    <tr>
+                      <th>Address</th>
+                      <td>{{ vendorItem.address || 'Not Available' }}</td>
+                    </tr>
+                    <tr>
+                      <th>Email</th>
+                      <td>{{ vendorItem.email || 'Not Available' }}</td>
+                    </tr>
+                    <tr>
+                      <th>Gender</th>
+                      <td>{{ vendorItem.gender || 'Not Available' }}</td>
+                    </tr>
+                    <tr>
+                      <th>Phone Number</th>
+                      <td>{{ vendorItem.phone_number || 'Not Available' }}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div class="modal-footer justify-content-center">
+              <button
+                @click="edit(vendorItem)"
+                class="text-center btn bg-default"
+                v-if="!viewCategory"
+              >
+                Edit
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div v-if="pageParams.next_page_url || pageParams.prev_page_url">
+        <base-pagination :page-param="pageParams" @fetchData="fetchData">
+        </base-pagination>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+import { get, patch } from '../../utilities/api'
+import Vue from 'vue'
+
+import Flash from '../../utilities/flash'
+
+import { mapGetters, mapActions } from 'vuex'
+import CustomHeader from '../../components/customHeader'
+import ZeroState from '../../components/ZeroState.vue'
+
+import BasePagination from '../../components/Pagination/BasePagination'
+import ResueableSearch from '../../components/ReusableSearch.vue'
+import AddVendorsModal from '../../components/modals/AddVendorsModal.vue'
+import EditVendorsModal from '../../components/modals/EditVendorsModal.vue'
+
+export default {
+  props: {
+    //TODO::verify if its necessary to make this a prop
+    withBranchFilter: { default: true },
+    urlToFetchOrders: { default: '/api/brand' },
+  },
+
+  components: { CustomHeader, BasePagination, ResueableSearch, ZeroState },
+
+  computed: { ...mapGetters(['getBranches']) },
+
+  data() {
+    return {
+      apiUrl: `${process.env.VUE_APP_BNPL_URL}/api/all/vendors`,
+      branch_id: '',
+      showCat: false,
+      OId: null,
+      viewCategory: false,
+      showModalContent: false,
+      pageParams: {},
+      page_size: 10,
+      date_from: null,
+      date_to: null,
+      page: 1,
+      filters: [
+        { name: 'date from', model: 'date_from' },
+        { name: 'date to', model: 'date_to' },
+      ],
+      vendors: [],
+
+      vendorItem: null,
+      response: {},
+      show: false,
+      headings: ['Name', 'Vendor ID', 'Email', 'Date Joined'],
+      searchColumns: [{ title: 'Name', column: 'name' }],
+    }
+  },
+
+  methods: {
+    fetchData() {
+      this.$scrollToTop()
+      this.$LIPS(true)
+      let { page, page_size } = this.$data
+      get(
+        `${this.apiUrl}${
+          !!this.pageParams.page ? `?page=${this.pageParams.page}` : ''
+        }` +
+          `${!!this.pageParams.limit ? `&limit=${this.pageParams.limit}` : ''}`
+      )
+        .then(({ data }) => this.prepareList(data))
+        .catch(() => Flash.setError('Error Preparing form'))
+    },
+
+    prepareList(response) {
+      let {
+        current_page,
+        first_page_url,
+        from,
+        last_page,
+        last_page_url,
+        data,
+        per_page,
+        next_page_url,
+        to,
+        total,
+        prev_page_url,
+      } = response.result.vendors
+      this.pageParams = Object.assign({}, this.pageParams, {
+        current_page,
+        first_page_url,
+        from,
+        last_page,
+        last_page_url,
+        per_page,
+        next_page_url,
+        to,
+        total,
+        prev_page_url,
+      })
+      this.vendors = data
+      this.OId = from
+      this.$LIPS(false)
+    },
+
+    next(firstPage = null) {
+      if (this.pageParams.next_page_url) {
+        this.page = firstPage ? firstPage : parseInt(this.page) + 1
+        this.fetchData()
+      }
+    },
+
+    prev(lastPage = null) {
+      if (this.pageParams.prev_page_url) {
+        this.page = lastPage ? lastPage : parseInt(this.page) - 1
+        this.fetchData()
+      }
+    },
+
+    showCategory(item) {
+      this.showModalContent = true
+      this.brandItem = item
+      this.viewCategory = true
+      return $(`#viewBrand`).modal('toggle')
+    },
+    addCat(brand, category) {
+      if (brand.categories.some(cat => cat.id === category.id)) {
+        alert(`${category.name} category already exists`)
+      } else {
+        brand.categories.push(category)
+        this.categories = this.categories.filter(function(item, index, arr) {
+          return category.id !== item.id
+        })
+      }
+    },
+
+    toggleCat() {
+      Vue.set(this.$data, 'showCat', !this.showCat)
+    },
+
+    createVendor() {
+      this.$modal.show(
+        AddVendorsModal,
+        {},
+        {
+          draggable: false,
+          height: 'auto',
+          clickToClose: false,
+        }
+      )
+    },
+
+    viewVendor(vendor) {
+      this.showModalContent = true
+      this.vendorItem = vendor
+      return $(`#viewBrand`).modal('toggle')
+    },
+    edit(item) {
+      this.showModalContent = false
+      $(`#viewBrand`).modal('toggle')
+      this.$modal.show(
+        EditVendorsModal,
+        { vendor: this.vendorItem },
+        {
+          draggable: false,
+          height: 'auto',
+          clickToClose: false,
+        }
+      )
+    },
+
+    searchEvent(data) {
+      get(this.urlToFetchOrders + data)
+        .then(({ data }) => this.prepareList(data))
+        .catch(() => Flash.setError('Error Preparing form'))
+    },
+
+    ...mapActions('ModalAccess', [
+      'addCustomerOptionsModalsToDom',
+      'removeCustomerOptionsModalsFromDom',
+    ]),
+  },
+
+  created() {
+    this.$props.withBranchFilter &&
+      this.filters.unshift({ name: 'branch', model: 'branch_id' })
+    this.addCustomerOptionsModalsToDom()
+    this.$prepareBranches()
+    // this.fetchData()
+  },
+
+  destroyed() {
+    this.removeCustomerOptionsModalsFromDom()
+  },
+  filters: {
+    status: function(value) {
+      return value === 1 ? 'Active' : 'Inactive'
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>

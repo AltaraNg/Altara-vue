c3f4abcbdebe93c5e63b468fb62dcd0bdc3fcd44
Omodamola <darmhoo@hotmail.com>
2023-03-27 18:28:52 +0100

    add bnpl access (#431)

--- src/utilities/api.js
+++ src/utilities/api.js
@@ -2,24 +2,26 @@ import axios from 'axios'
 import Auth from './auth'
 
 axios.defaults.baseURL = process.env.VUE_APP_URL;
-export const get = (url, params = {}) => axios({ method: 'GET', url, params: params ,headers: { 'Authorization': `Bearer ${Auth.state.api_token}`, 'BNLP-ADMIN-ACCESS': process.env.VUE_APP_BNPL_ACCESS_KEY  } });
+
+let user_id = localStorage.getItem('user_id');
+export const get = (url, params = {}) => axios({ method: 'GET', url, params: params ,headers: { 'Authorization': `Bearer ${Auth.state.api_token}`, 'BNLP-ADMIN-ACCESS': process.env.VUE_APP_BNPL_ACCESS_KEY, 'BNLP-ADMIN-ACCESS-AUTH-USER-ID': user_id  } });
 export const post = (url, data) => axios({
     method: 'POST',
     url,
     data,
-    headers: { 'Authorization': `Bearer ${Auth.state.api_token}`, 'BNLP-ADMIN-ACCESS': process.env.VUE_APP_BNPL_ACCESS_KEY  }
+    headers: { 'Authorization': `Bearer ${Auth.state.api_token}`, 'BNLP-ADMIN-ACCESS': process.env.VUE_APP_BNPL_ACCESS_KEY, 'BNLP-ADMIN-ACCESS-AUTH-USER-ID': user_id  }
 });
 export const put = (url, data) => axios({
     method: 'PUT',
     url,
     data,
-    headers: { 'Authorization': `Bearer ${Auth.state.api_token}`, 'BNLP-ADMIN-ACCESS': process.env.VUE_APP_BNPL_ACCESS_KEY  }
+    headers: { 'Authorization': `Bearer ${Auth.state.api_token}`, 'BNLP-ADMIN-ACCESS': process.env.VUE_APP_BNPL_ACCESS_KEY, 'BNLP-ADMIN-ACCESS-AUTH-USER-ID': user_id  }
 });
 export const patch = (url, data) => axios({
     method: 'PATCH',
     url,
     data,
-    headers: { 'Authorization': `Bearer ${Auth.state.api_token}`, 'BNLP-ADMIN-ACCESS': process.env.VUE_APP_BNPL_ACCESS_KEY  }
+    headers: { 'Authorization': `Bearer ${Auth.state.api_token}`, 'BNLP-ADMIN-ACCESS': process.env.VUE_APP_BNPL_ACCESS_KEY, 'BNLP-ADMIN-ACCESS-AUTH-USER-ID': user_id  }
 });
 export const byMethod = (method, url, data, params = {}) => axios({
     method,

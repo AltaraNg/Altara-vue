cad7e6d79313c81a3c29c8277897dc110a612bb4
JoshAltCrd <jojoawo@altaracredit.com>
2023-07-28 11:24:39 +0100

    chore: update to have informal view

--- src/components/modals/ViewVerificationCheckList.vue
+++ src/components/modals/ViewVerificationCheckList.vue
@@ -16,7 +16,7 @@
         </span>
       </div>
     </div>
-    <div class="modal-body">
+    <div class="modal-body" v-if="verification.status === 'Formal' ">
       <ol class="list-group">
         <h5>Generic</h5>
         <li class="list-group-item my-3">
@@ -225,6 +225,252 @@
           >
           <p class="display-text">
             {{ verification.dateOfSalary }}
+          </p>
+        </li>
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >Does the customer travel often?</label
+          >
+          <p class="display-text">
+            {{ verification.oftenTravel }}
+          </p>
+        </li>
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >Utility Bill OR Rent receipt OR Water bill provided? Does the address match the house address on the form?</label
+          >
+          <p class="display-text">
+            {{ verification.receipt }}
+          </p>
+        </li>
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >Who did you see/speak to when you got to the office?</label
+          >
+          <p class="display-text">
+            {{ verification.whoYouSpokeWith }}
+          </p>
+        </li>
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >Based on the feedback from collegues, How long has the customer worked there?</label
+          >
+          <p class="display-text">
+            {{ verification.customerWorkDuration }}
+          </p>
+        </li>
+        
+      </ol>
+    </div>
+    <div class="modal-body" v-if="verification.status === 'Informal' ">
+      <ol class="list-group">
+        <h5>Generic</h5>
+        <li class="list-group-item my-3">
+          <label for="checklist1" class=""
+            >What location was visited and verified?</label
+          >
+          <p class="display-text">
+            {{ verification.location }}
+          </p>
+        </li>
+        <li class="list-group-item my-3">
+          <label for="checklist">If other, Specify</label>
+          <p class="display-text">
+            {{ verification.otherOption || 'N/A' }}
+          </p>
+        </li>
+
+        <li class="list-group-item my-3">
+          <label for="checklist1" class=""
+            >Does the address match what was given on the application Form?</label
+          >
+          <p class="display-text">
+            {{ verification.addressMatch }}
+          </p>
+        </li>
+
+        <li class="list-group-item my-3">
+          <label for="checklist1" class=""
+            >Distance to the customer's house/office/ business/shop</label
+          >
+          <p class="display-text">
+            {{ verification.customerDistance }}
+          </p>
+        </li>
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlSelect2"
+            >Is customer married?</label
+          >
+          <p class="display-text">
+            {{ verification.married }}
+          </p>
+        </li>
+
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlSelect2"
+            >If Yes - What is the name of their husband / wife and what is their number</label
+          >
+          <p class="display-text">
+            {{ verification.nameOfSpouse }}
+          </p>
+        </li>
+        <li class="list-group-item">
+          <label for="exampleFormControlTextarea1"
+            >How many children does the customer have?</label
+          >
+          <p class="display-text">
+            {{ verification.customerChildren }}
+          </p>
+        </li>
+
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlSelect2"
+            >Is the customer's spouse salary earner or have business?</label
+          >
+          <p class="display-text">
+            {{ verification.spouseSalary }}
+          </p>
+        </li>
+
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlSelect2"
+            >Guarantor gave the same house address/workplace address as the customer?</label
+          >
+          <p class="display-text">
+            {{ verification.addressByGuarantor }}
+          </p>
+        </li>
+
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >Picture or video of collaterals incase of loan</label
+          >
+          <p class="display-text">
+            {{ verification.collateralPictures }}
+          </p>
+        </li>
+
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >Customer has provided a picture of his accomodation/workplace  and it is the same as what was verified by the VCR?</label
+          >
+          <p class="display-text">
+            {{ verification.accommodationPicture }}
+          </p>
+        </li>
+
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >Customer has provided their BVN?</label
+          >
+          <p class="display-text">
+            {{ verification.hasBvn }}
+          </p>
+        </li>
+
+        <li class="list-group-item my-3">
+          <label for="checklist">None of the Guarantors are the spouse of the customer</label>
+          <p class="display-text">
+            {{ verification.spouseNotGuarantor }}
+          </p>
+        </li>
+
+        <div class="spaceAfter"></div>
+        <h5>Home</h5>
+        <li class="list-group-item my-3">
+          <label for="checklist">How long has the customer lived at his/her location?</label>
+          <p class="display-text">
+            {{ verification.locationDuration }}
+          </p>
+        </li>
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >Utility Bill OR Rent receipt OR Water bill provided? Does the address match the house address on the form?</label
+          >
+          <p class="display-text">
+            {{ verification.bill }}
+          </p>
+        </li>
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >Does the customer travel often?</label
+          >
+          <p class="display-text">
+            {{ verification.customerTravels }}
+          </p>
+        </li>
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >What is the nature of the home addess (location description? how does it look? Is it obvious the customer lives there? Family pictures ?)</label
+          >
+          <p class="display-text">
+            {{ verification.natureOfHomeAddress }}
+          </p>
+        </li>
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >Who did you see at the house/speak to when you got to the house?</label
+          >
+          <p class="display-text">
+            {{ verification.whoYouSpeakTo }}
+          </p>
+        </li>
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >Does the customer own their house?</label
+          >
+          <p class="display-text">
+            {{ verification.ownHouse }}
+          </p>
+        </li>
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >Based on the feedback from neighbours, How long has the customer lived there?</label
+          >
+          <p class="display-text">
+            {{ verification.houseLivedDuration }}
+          </p>
+        </li>
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >[Ask Question to neighbour] Have they had any issues with the neighbours or anyone in the area?</label
+          >
+          <p class="display-text">
+            {{ verification.issueWithNeighbour }}
+          </p>
+        </li>
+        <div class="spaceAfter"></div>
+        <h5>Office/ Business</h5>
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >"How many years has the customer owned thier business ( >1 year) ?  if Shop/Business is verified</label
+          >
+          <p class="display-text">
+            {{ verification.location }}
+          </p>
+        </li>
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >Have we met with the sellers/co-workers and do they speak well of the customer?</label
+          >
+          <p class="display-text">
+            {{ verification.metManager }}
+          </p>
+        </li>
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >What is the customers's business?</label
+          >
+          <p class="display-text">
+            {{ verification.customerWork }}
+          </p>
+        </li>
+        <li class="list-group-item my-3">
+          <label for="exampleFormControlTextarea1"
+            >How often does the customer make sales? (Sales trend/is it seasonal)</label
+          >
+          <p class="display-text">
+            {{ verification.customerSales }}
           </p>
         </li>
         <li class="list-group-item my-3">
